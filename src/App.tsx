8cf8831aca5ab67ccaf8dd0b3ffa3892be22c1ea
Samuel Gonzalez <samglz10@gmail.com>
2023-09-21 22:14:08 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Home from './components/home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <Home/>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
